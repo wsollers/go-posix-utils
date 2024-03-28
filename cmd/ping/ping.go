e89f817e70f4f3bc962bcc34d63d009431961a61
BoondockTaints <wsollers@gmail.com>
2024-03-28 14:34:14 -0400

    local changes
    local changes

--- cmd/ping/ping.go
+++ cmd/ping/ping.go
@@ -2,244 +2,229 @@ package cmd
 
 import (
 	"fmt"
-  "strconv"
-  "log"
+	"log"
+	"net"
 	"os"
-  "net"
-  //"net/netip"
+	"strconv"
+	//"net/netip"
 	//"runtime"
 	"golang.org/x/net/icmp"
-  ///"net"
-  //"golang.org/x/net/icmp.Echo"
+	///"net"
+	//"golang.org/x/net/icmp.Echo"
 	"golang.org/x/net/ipv4"
 	//"golang.org/x/net/ipv6"
-  
 
-//"syscall"
+	//"syscall"
 
 	"github.com/spf13/cobra"
 	//"github.com/spf13/viper"
-  //"golang.org/x/sys/unix"
+	//"golang.org/x/sys/unix"
 )
 
 // https://datatracker.ietf.org/doc/html/rfc792
 // /usr/sbin/ping host [timeout]
-// /usr/sbin/ping  -s [-l | -U]  [-adlLnrRv] [-A addr_family] 
+// /usr/sbin/ping  -s [-l | -U]  [-adlLnrRv] [-A addr_family]
 //                 [-c traffic_class] [-g gateway  [ -g	gateway...]]
 //                 [-F flow_label] [-I interval]
 
-var host string
-var timeout int
+var (
+	host    string
+	timeout int
+)
 
 var PingCmd = &cobra.Command{
-  Use:   "ping",
-  Short: "Ping the server",
-  Long:  "Ping the server",
-  Run: func(cmd *cobra.Command, args []string) {
-    fmt.Println("ping called")
+	Use:   "ping",
+	Short: "Ping the server",
+	Long:  "Ping the server",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("ping called")
 
-    if len(args) < 1 {
-      log.Fatal("usage: ping host [timeout]")
-    }
-    host = args[0]
-    if len(host) < 1 {
-      log.Fatal("No host provided")
-    }
-    err := error(nil)
-    timeout, err = strconv.Atoi(args[1])
-    if err != nil {
-      log.Fatal("Invalid timeout")
-    }
-    if timeout < 1 {
-      log.Fatal("Invalid timeout")
-    }
-    fmt.Println("host:", host)
-    if host == "" {
-      log.Fatal("No host provided")
-    }
-  
-    ping()
-  },
+		if len(args) < 1 {
+			log.Fatal("usage: ping host [timeout]")
+		}
+		host = args[0]
+		if len(host) < 1 {
+			log.Fatal("No host provided")
+		}
+		err := error(nil)
+		timeout, err = strconv.Atoi(args[1])
+		if err != nil {
+			log.Fatal("Invalid timeout")
+		}
+		if timeout < 1 {
+			log.Fatal("Invalid timeout")
+		}
+		fmt.Println("host:", host)
+		if host == "" {
+			log.Fatal("No host provided")
+		}
+
+		ping()
+	},
 }
 
 func ping() {
-  fmt.Println("Pinging server...")
-  c, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
-  if err != nil {
-    log.Fatalf("listen err, %s", err)
-  }
-  defer c.Close()
-
-  ip, err := net.ResolveIPAddr("ip", host)
-  if err != nil {
-    log.Fatal("ResolveIPAddr: ",err)
-  }
-  wm := icmp.Message{
-    Type: ipv4.ICMPTypeEcho, Code: 0,
-    Body: &icmp.Echo{
-     ID: os.Getpid() & 0xffff, Seq: 1,
-     Data: []byte("HELLO-R-U-THERE"),
-    },
-  }
-  wb, err := wm.Marshal(nil)
-  if err != nil {
-    log.Fatal(err)
-  }
-  if _, err := c.WriteTo(wb, &net.IPAddr{IP: net.ParseIP(ip.IP.String())}); err != nil {
-    log.Fatalf("WriteTo err, %s", err)
-  }
-
-  rb := make([]byte, 1500)
-  n, peer, err := c.ReadFrom(rb)
-  if err != nil {
-    log.Fatal(err)
-    }
-    rm, err := icmp.ParseMessage(ipv4.ICMPTypeEchoReply.Protocol(), rb[:n])
-    if err != nil {
-        log.Fatal(err)
-    }
-    switch rm.Type {
-    case ipv4.ICMPTypeEchoReply:
-        log.Printf("got reflection from %v", peer)
-      fmt.Println();
-      fmt.Println("Packet received from: ", rm)
-    default:
-        log.Printf("got %+v; want echo reply", rm)
-    }
-/*
-  c, err := icmp.ListenPacket("udp4", "0.0.0.0")
+	fmt.Println("Pinging server...")
+	c, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("listen err, %s", err)
 	}
 	defer c.Close()
 
- 	hostname, err := os.Hostname()
-  if err != nil {
-    log.Fatal("Hostname: ", err)
-  }
-  ip, err := net.ResolveIPAddr("ip", host)
-  if err != nil {
-    fmt.Println("ResolveIPAddr: ",err)
-  }
-	fmt.Println("Host: ", host)
-  fmt.Println("ip: ", ip)
-	fmt.Println("Hostname: ", hostname)
-
-  serverFD, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, unix.IPPROTO_ICMP)
+	ip, err := net.ResolveIPAddr("ip", host)
 	if err != nil {
-		log.Fatal("Socket: ", err)
+		log.Fatal("ResolveIPAddr: ", err)
 	}
-  defer unix.Close(serverFD)
-*/
-  /*
-  */
-  /*
-  addrRaw1 := net.ParseIP(host)
-  addrRaw2 := addrRaw1.To4()
-  var byteArray [4]byte
-  copy (byteArray[:], addrRaw2.To4())
-  serverAddr := &unix.SockaddrInet4{
-		Port: 0,
-		Addr: byteArray,
-	}
-  /*
-  err = unix.Bind(serverFD, serverAddr)
-	if err != nil {
-		log.Fatal("Bind: ", err)
-	}
-	fmt.Printf("Server: Bound to addr: %d, port: %d\n", serverAddr.Addr, serverAddr.Port)
-  */
-  /*
-  err = unix.Connect(serverFD, serverAddr)
-	if err != nil {
-		if err == unix.ECONNREFUSED {
-      log.Fatal("* Connection failed: ", err)
-		}
-	}
-  fmt.Printf("Server: Connected to addr: %d, port: %d\n", serverAddr.Addr, serverAddr.Port)
-  /*
-  server, err := net.ResolveUDPAddr("udp4", ip.IP.String())
-  if err != nil {
-    log.Fatal("ResolveUDPAddr: ", err)
-  }
-  conn, err := net.DialUDP("udp4:icmp", nil, server)
-  if err != nil {
-    fmt.Println("Dial Error: ", err)
-    return
-  }
-  defer conn.Close()
-	wm := icmp.Message {
-		Type: ipv4.ICMPTypeEcho,
-    Code: 0,
-		Body: &icmp.Echo {
-		  ID: os.Getpid() & 0xffff, 
-      Seq: 1,
-		  Data: []byte("HELLO-R-U-THERE"),
+	wm := icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Body: &icmp.Echo{
+			ID: os.Getpid() & 0xffff, Seq: 1,
+			Data: []byte("HELLO-R-U-THERE"),
 		},
 	}
-
 	wb, err := wm.Marshal(nil)
 	if err != nil {
-    log.Fatal("Marshal: ", err)
+		log.Fatal(err)
 	}
-  if _, err := conn.Write(wb); err != nil {
-    log.Fatal("Write Error: ", err)
+	if _, err := c.WriteTo(wb, &net.IPAddr{IP: net.ParseIP(ip.IP.String())}); err != nil {
+		log.Fatalf("WriteTo err, %s", err)
 	}
 
 	rb := make([]byte, 1500)
 	n, peer, err := c.ReadFrom(rb)
 	if err != nil {
-    log.Fatal("ReadFrom Error: ", err)
+		log.Fatal(err)
 	}
-	rm, err := icmp.ParseMessage(58, rb[:n])
+	rm, err := icmp.ParseMessage(ipv4.ICMPTypeEchoReply.Protocol(), rb[:n])
 	if err != nil {
-    log.Fatal("Parse Message: ", err)
+		log.Fatal(err)
 	}
 	switch rm.Type {
-	  case ipv6.ICMPTypeEchoReply:
-      log.Printf("IPV6: got reflection from %v", peer)
-    case ipv4.ICMPTypeEchoReply:
-      log.Printf("IPV4: got reflection from %v", peer)
-	  default:
-	  	log.Printf("got %+v; want echo reply", rm)
+	case ipv4.ICMPTypeEchoReply:
+		log.Printf("got reflection from %v", peer)
+		fmt.Println()
+		fmt.Println("Packet received from: ", rm)
+	default:
+		log.Printf("got %+v; want echo reply", rm)
 	}
-*/
+	/*
+	     c, err := icmp.ListenPacket("udp4", "0.0.0.0")
+	   	if err != nil {
+	   		log.Fatal(err)
+	   	}
+	   	defer c.Close()
 
+	    	hostname, err := os.Hostname()
+	     if err != nil {
+	       log.Fatal("Hostname: ", err)
+	     }
+	     ip, err := net.ResolveIPAddr("ip", host)
+	     if err != nil {
+	       fmt.Println("ResolveIPAddr: ",err)
+	     }
+	   	fmt.Println("Host: ", host)
+	     fmt.Println("ip: ", ip)
+	   	fmt.Println("Hostname: ", hostname)
 
+	     serverFD, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, unix.IPPROTO_ICMP)
+	   	if err != nil {
+	   		log.Fatal("Socket: ", err)
+	   	}
+	     defer unix.Close(serverFD)
+	*/
+	/*
+	 */
+	/*
+		  addrRaw1 := net.ParseIP(host)
+		  addrRaw2 := addrRaw1.To4()
+		  var byteArray [4]byte
+		  copy (byteArray[:], addrRaw2.To4())
+		  serverAddr := &unix.SockaddrInet4{
+				Port: 0,
+				Addr: byteArray,
+			}
+		  /*
+		  err = unix.Bind(serverFD, serverAddr)
+			if err != nil {
+				log.Fatal("Bind: ", err)
+			}
+			fmt.Printf("Server: Bound to addr: %d, port: %d\n", serverAddr.Addr, serverAddr.Port)
+	*/
+	/*
+		  err = unix.Connect(serverFD, serverAddr)
+			if err != nil {
+				if err == unix.ECONNREFUSED {
+		      log.Fatal("* Connection failed: ", err)
+				}
+			}
+		  fmt.Printf("Server: Connected to addr: %d, port: %d\n", serverAddr.Addr, serverAddr.Port)
+		  /*
+		  server, err := net.ResolveUDPAddr("udp4", ip.IP.String())
+		  if err != nil {
+		    log.Fatal("ResolveUDPAddr: ", err)
+		  }
+		  conn, err := net.DialUDP("udp4:icmp", nil, server)
+		  if err != nil {
+		    fmt.Println("Dial Error: ", err)
+		    return
+		  }
+		  defer conn.Close()
+			wm := icmp.Message {
+				Type: ipv4.ICMPTypeEcho,
+		    Code: 0,
+				Body: &icmp.Echo {
+				  ID: os.Getpid() & 0xffff,
+		      Seq: 1,
+				  Data: []byte("HELLO-R-U-THERE"),
+				},
+			}
 
+			wb, err := wm.Marshal(nil)
+			if err != nil {
+		    log.Fatal("Marshal: ", err)
+			}
+		  if _, err := conn.Write(wb); err != nil {
+		    log.Fatal("Write Error: ", err)
+			}
 
+			rb := make([]byte, 1500)
+			n, peer, err := c.ReadFrom(rb)
+			if err != nil {
+		    log.Fatal("ReadFrom Error: ", err)
+			}
+			rm, err := icmp.ParseMessage(58, rb[:n])
+			if err != nil {
+		    log.Fatal("Parse Message: ", err)
+			}
+			switch rm.Type {
+			  case ipv6.ICMPTypeEchoReply:
+		      log.Printf("IPV6: got reflection from %v", peer)
+		    case ipv4.ICMPTypeEchoReply:
+		      log.Printf("IPV4: got reflection from %v", peer)
+			  default:
+			  	log.Printf("got %+v; want echo reply", rm)
+			}
+	*/
 
+	/*
+	  pkt := icmp.Echo {
+	    ID: 1,
+	    Seq: 1,
+	    Data: []byte("Hello"),
+	  }
+	  fmt.Println("pkt:", pkt)
 
+	  for {
+	    rawpkt, err := pkt.Marshal(3)
+	    if err != nil {
+	      Log.Fatal("Error marshalling packet")
+	    }
 
-
-
-
-
-
-
-
-
-  /*
-  pkt := icmp.Echo {
-    ID: 1,
-    Seq: 1,
-    Data: []byte("Hello"),
-  }
-  fmt.Println("pkt:", pkt)
-
-  for {
-    rawpkt, err := pkt.Marshal(3)
-    if err != nil {
-      Log.Fatal("Error marshalling packet")
-    }
-
-    // send packet
-    // wait for response
-    // print response
-  }
-  // we have a host and a timeout
-  // build a raw icmp ping packet and send it to the specified host
-  */
+	    // send packet
+	    // wait for response
+	    // print response
+	  }
+	  // we have a host and a timeout
+	  // build a raw icmp ping packet and send it to the specified host
+	*/
 }
-
-
--- main.go
+++ main.go
@@ -1,80 +1,77 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
+	"fmt"
+	"log"
+	"net/http"
 
-  "github.com/spf13/cobra"
-  //"github.com/spf13/viper"
+	"github.com/spf13/cobra"
+	//"github.com/spf13/viper"
 
-  ping "github.com/wsollers/go-posix-utils/cmd/ping"
-  web   "github.com/wsollers/go-posix-utils/cmd/web"
-
+	ping "github.com/wsollers/go-posix-utils/cmd/ping"
+	web "github.com/wsollers/go-posix-utils/cmd/web"
 )
 
-
 var rootCmd = &cobra.Command{
-  Use:   "go-posix-utils [sub]",
-  Short: "golang reimplementation of some posix utilities",
-  PersistentPreRun: func(cmd *cobra.Command, args []string) {
-    fmt.Printf("Inside rootCmd PersistentPreRun with args: %v\n", args)
-  },
-  PreRun: func(cmd *cobra.Command, args []string) {
-    fmt.Printf("Inside rootCmd PreRun with args: %v\n", args)
-  },
-  Run: func(cmd *cobra.Command, args []string) {
-    fmt.Printf("Inside rootCmd Run with args: %v\n", args)
-  },
-  PostRun: func(cmd *cobra.Command, args []string) {
-    fmt.Printf("Inside rootCmd PostRun with args: %v\n", args)
-  },
-  PersistentPostRun: func(cmd *cobra.Command, args []string) {
-    fmt.Printf("Inside rootCmd PersistentPostRun with args: %v\n", args)
-  },
+	Use:   "go-posix-utils [sub]",
+	Short: "golang reimplementation of some posix utilities",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Inside rootCmd PersistentPreRun with args: %v\n", args)
+	},
+	PreRun: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Inside rootCmd PreRun with args: %v\n", args)
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Inside rootCmd Run with args: %v\n", args)
+	},
+	PostRun: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Inside rootCmd PostRun with args: %v\n", args)
+	},
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Inside rootCmd PersistentPostRun with args: %v\n", args)
+	},
 }
 
 func main() {
-    
-  rootCmd.AddCommand(ping.PingCmd)
-  rootCmd.AddCommand(web.WebServCmd)
+	rootCmd.AddCommand(ping.PingCmd)
+	rootCmd.AddCommand(web.WebServCmd)
 
-  if err := rootCmd.Execute(); err != nil {
-    log.Fatal(err)
-  }
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
 
-//  ping.PingCmd.Run(nil, nil)
-  /*
-  var subCmd = &cobra.Command{
-    Use:   "sub [no options!]",
-    Short: "My subcommand",
-    PreRun: func(cmd *cobra.Command, args []string) {
-      fmt.Printf("Inside subCmd PreRun with args: %v\n", args)
-    },
-    Run: func(cmd *cobra.Command, args []string) {
-      fmt.Printf("Inside subCmd Run with args: %v\n", args)
-    },
-    PostRun: func(cmd *cobra.Command, args []string) {
-      fmt.Printf("Inside subCmd PostRun with args: %v\n", args)
-    },
-    PersistentPostRun: func(cmd *cobra.Command, args []string) {
-      fmt.Printf("Inside subCmd PersistentPostRun with args: %v\n", args)
-    },
-  }
+	//  ping.PingCmd.Run(nil, nil)
+	/*
+	  var subCmd = &cobra.Command{
+	    Use:   "sub [no options!]",
+	    Short: "My subcommand",
+	    PreRun: func(cmd *cobra.Command, args []string) {
+	      fmt.Printf("Inside subCmd PreRun with args: %v\n", args)
+	    },
+	    Run: func(cmd *cobra.Command, args []string) {
+	      fmt.Printf("Inside subCmd Run with args: %v\n", args)
+	    },
+	    PostRun: func(cmd *cobra.Command, args []string) {
+	      fmt.Printf("Inside subCmd PostRun with args: %v\n", args)
+	    },
+	    PersistentPostRun: func(cmd *cobra.Command, args []string) {
+	      fmt.Printf("Inside subCmd PersistentPostRun with args: %v\n", args)
+	    },
+	  }
 
-  rootCmd.AddCommand(subCmd)
+	  rootCmd.AddCommand(subCmd)
 
-  rootCmd.SetArgs([]string{""})
-  rootCmd.Execute()
-  fmt.Println()
-  rootCmd.SetArgs([]string{"sub", "arg1", "arg2"})
-  rootCmd.Execute()
-  */
-  http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hello, World!")
-    })
-    fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatal(err)
-    }
+	  rootCmd.SetArgs([]string{""})
+	  rootCmd.Execute()
+	  fmt.Println()
+	  rootCmd.SetArgs([]string{"sub", "arg1", "arg2"})
+	  rootCmd.Execute()
+	*/
+	http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+	fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
